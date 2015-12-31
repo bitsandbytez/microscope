0c7f5e035010f11e4d3315682d579a48ce4ff405
Mike <mike.c.bobadilla@gmail.com>
2015-12-30 21:26:48 -0800

    Denied access to new posts page when not logged in.

--- client/templates/posts/post_submit.js
+++ client/templates/posts/post_submit.js
@@ -1,13 +1,13 @@
 Template.postSubmit.events({
-  "submit form": function(event){
+  'submit form': function(event){
      event.preventDefault();
 
-     var post - {
-       url: $(event.target).find('[name=url]').val();
-       title: $(event.target).find('[name=title]').val();
+     var post = {
+       url: $(event.target).find('[name=url]').val(),
+       title: $(event.target).find('[name=title]').val()
      };
 
-     post._id = Posts.inset(post);
+     post._id = Posts.insert(post);
      Router.go('postPage', post);
   }
 });
