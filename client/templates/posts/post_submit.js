2bd18bbbf908358ddaff229c02b12e443145cf8f
Mike <mike.c.bobadilla@gmail.com>
2015-12-31 11:22:01 -0800

    Validate post contents on submission.

--- client/templates/posts/post_submit.js
+++ client/templates/posts/post_submit.js
@@ -7,12 +7,17 @@ Template.postSubmit.events({
        title: $(event.target).find('[name=title]').val()
      };
 
+     var errors = validatePost(post);
+     if(errors.title || errors.url) {
+       return Session.set('postSubmitErrors', errors);
+     }
+
      Meteor.call("postInsert", post, function(error, result){
        // display the error to the user and abort
        if(error){
          return throwError(error.reason);
        }
-
+       
        // show this result but route anyway
        if(result.postExists) {
          throwError('This link has already been posted');
@@ -21,5 +26,19 @@ Template.postSubmit.events({
        Router.go('postPage', { _id: result._id });
      });
 
+  }
+});
+
+Template.postSubmit.onCreated(function () {
+  Session.set('postSubmitErrors', {});
+});
+
+
+Template.postSubmit.helpers({
+  errorMessage: function(field){
+     return Session.get('postSubmitErrors')[field];
+  },
+  errorClass: function(field){
+     return !!Session.get('postSubmitErrors')[field] ? 'has-error' : '';
   }
 });
